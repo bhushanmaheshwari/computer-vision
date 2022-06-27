5e5cf024fdfe31377f80058da5ae321497476e92
Bhushan Maheshwari <bmaheshwari@worldbankgroup.org>
2022-06-27 16:36:28 -0400

    azure api calls integrated

--- app.py
+++ app.py
@@ -2,37 +2,38 @@ from flask import Flask, render_template,  request
 from werkzeug.utils import secure_filename
 import os
 import sqlite3 as sql
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
 
-
+# sqlite
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads'
 
-
 conn = sql.connect('database.db')
-conn.execute('CREATE TABLE history_dev (name TEXT, path TEXT)')
+conn.execute(
+    'CREATE TABLE IF NOT EXISTS history_db_dev (name TEXT, path TEXT, description TEXT, tags TEXT, color TEXT)')
 conn.close()
+
+# azure computer vision api
+key = '76a73a58680a4d45ad7f9428c572fa27'
+endpoint = 'https://cogser-one.cognitiveservices.azure.com/'
+
+credentials = CognitiveServicesCredentials(key)
+client = ComputerVisionClient(
+    endpoint=endpoint,
+    credentials=credentials
+)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
-@app.route('/history')
-def history():
     con = sql.connect("database.db")
     con.row_factory = sql.Row
-   
     cur = con.cursor()
-    cur.execute("select * from history_dev")
-   
-    rows = cur.fetchall(); 
-    return render_template('history.html', rows=rows)
-
-
-@app.route('/new')
-def new():
-    return render_template('new.html')
+    cur.execute("select * from history_db_dev")
+    rows = cur.fetchall()
+    return render_template('index.html',  rows=rows)
 
 
 @app.route('/details', methods=['GET', 'POST'])
@@ -45,19 +46,39 @@ def upload_file():
         )
         f.save(path)
 
+        with open(path, "rb") as image_stream:
+            image_analysis = client.analyze_image_in_stream(
+                image=image_stream,
+                visual_features=[
+                    VisualFeatureTypes.image_type,  # Could use simple str "ImageType"
+                    VisualFeatureTypes.faces,      # Could use simple str "Faces"
+                    VisualFeatureTypes.categories,  # Could use simple str "Categories"
+                    VisualFeatureTypes.color,      # Could use simple str "Color"
+                    VisualFeatureTypes.tags,       # Could use simple str "Tags"
+                    VisualFeatureTypes.description  # Could use simple str "Description"
+                ]
+            )
+
+        description = image_analysis.description.captions[0].text
+        tags = ''
+        for tag in image_analysis.tags:
+            tags += ("{}\t\t{}".format(tag.name, tag.confidence)) + ', '
+
+        color = image_analysis.color.dominant_colors
+
         try:
             with sql.connect("database.db") as con:
                 cur = con.cursor()
                 cur.execute(
-                    "INSERT INTO history_dev(name, path) VALUES(?, ?)", (f.filename, path))
+                    "INSERT INTO history_db_dev(name, path, description, tags, color) VALUES(?, ?, ?, ?, ?)", (f.filename, path, description, tags, color))
+                print("Record successfully added")
                 con.commit()
-                msg = "Record successfully added"
         except:
             con.rollback()
             msg = "error in insert operation"
         finally:
             con.close()
-            return render_template('details.html', path=path)
+            return render_template('details.html', filename=f.filename, path=path, description=description, tags=tags, color=color)
 
 
 if __name__ == '__main__':
